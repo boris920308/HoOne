1198a0960fce22cceb93fae6c7a4c8132c4df854
boris920308 <jhoonj0308@gmail.com>
2023-05-24 18:35:36 +0900

    [Docs][Kotlin][Programmers][Lv1] no.12954, 주석 수정

--- hoon/kotlin/HelloHoon/src/main/kotlin/programmers/lv01/Lv1_12954_x_n.kt
+++ hoon/kotlin/HelloHoon/src/main/kotlin/programmers/lv01/Lv1_12954_x_n.kt
@@ -3,6 +3,8 @@ package main.kotlin.programmers.lv01
 
 /**
  *
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12954
+ *
  * 문제 설명
  * 함수 solution은 정수 x와 자연수 n을 입력 받아, x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴해야 합니다.
  * 다음 제한 조건을 보고, 조건을 만족하는 함수, solution을 완성해주세요.
